a9a598e9c64b6b7c74ce097df4640384e0534d81
unknown <1610893869@qq.com>
2016-07-05 10:06:31 +0800

    精简代码

--- text.py
+++ text.py
@@ -1,50 +1,10 @@
-# import time
-# def GetNowTime():
- 
-#     return time.strftime("%Y-%m-%d")
-# print(GetNowTime())
-# # print(time.localtime(time.time())))
-#-*-coding=gbk
-# 时间
-# import datetime,time
-# def GetNowTime():
-#      d1 = time.localtime(time.time())
-#      d2 = d1 + time.gmtime(tm_mon=1)
-#      return time.strftime("%Y-%m-%d")
-
-# print(GetNowTime())
+from selenium import webdriver
+from time import sleep
 
 
-print(max(1,2,34,65,2,))
-print(pow(3,4))
-print(ord('A')) 
-s = (x * x for x in range(10))
-for x in s:
-    print(x)
-s = (x * x for x in range(5))
-print(s)
-for x in s:
-    print(x)
-
-def fib(max):
-    n, a, b = 0, 0, 1
-    while n < max:
-        yield b
-        a, b = b, a + b
-        n = n + 1
-    return 'done'
-
-f = fib(10)
-print('fib(10):', f)
-for x in f:
-    print(x)
-
-# call generator manually:
-g = fib(5)
-while 1:
-    try:
-        x = next(g)
-        print('g:', x)
-    except StopIteration as e:
-        print('Generator return value:', e.value)
-        break
+brweb = webdriver.Chrome()
+brweb.get('https://www.baidu.com/')
+b=brweb.find_element_by_id('ul').find_elements_by_tag_name('a')
+for value in b:
+    value.click()
+    sleep(2)

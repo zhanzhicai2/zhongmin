f896001df1fb1b757aee80f3b5d70b6711fd11bd
zhanzhicai2 <1610893869@qq.com>
2018-08-20 08:38:26 +0800

    提交

--- new.py
+++ new.py
@@ -7,18 +7,34 @@
 #
 # if __name__ == '__main__':
 #   Test().f()
+# 深拷贝浅拷贝
 
-x = 1
-y = 2
-def add (x, y):
-    z = x + y
-    return z
-    
-print(add(x,y))
 
-x = 1
-y = 2
-def add (x, y):
-    z = x + y
-    print(z)
-print(add(x,y))
+import copy
+
+alist = [1, 2, 3, [12, 4]]
+
+# alist = (1, 2)
+
+b = alist
+print(alist)
+print(b)
+print(id(b))
+print(id(alist))
+
+
+# 浅拷贝
+
+
+b_copy = copy.copy(alist)
+alist[3].append(455)
+print(alist)
+print(b_copy)
+print(id(b_copy))
+print(id(alist))
+
+d_copy = copy.deepcopy(alist)
+print(alist)
+print(d_copy)
+print(id(d_copy))
+print(id(alist))
